e4c93cbef32b31869014ef3529dd5716b1bf6071
alloai <ca365qc@yahoo.com>
2019-07-21 18:41:10 -0600

    payroll module

--- hris/models.py
+++ hris/models.py
@@ -10,10 +10,10 @@ class Employee(models.Model):
     salary = models.DecimalField(max_digits = 16, decimal_places = 5)
     employee_number = models.IntegerField()
     hire_date = models.DateField()
-    street_no = models.CharField(max_length=10, help_text='Enter employee\'s address')
-    street_name = models.CharField(max_length=50, help_text='Enter employee\'s address')
-    city = models.CharField(max_length=50, help_text='Enter employee\'s address')
-    post_code = models.CharField(max_length=7, help_text='Enter employee\'s postal code')
+    street_no = models.CharField(max_length=10, default=' ',help_text='Enter employee\'s address')
+    street_name = models.CharField(max_length=50, default=' ',help_text='Enter employee\'s address')
+    city = models.CharField(max_length=50, default=' ', help_text='Enter employee\'s address')
+    post_code = models.CharField(max_length=7, default=' ',help_text='Enter employee\'s postal code')
     
     def __str__(self):
         return self.first_name + " " + self.last_name
