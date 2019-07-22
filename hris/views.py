45ab6ae1495409cd670f4c560f8e4fd68a3cb473
alloai <ca365qc@yahoo.com>
2019-07-21 18:21:00 -0600

    improved form

--- hris/views.py
+++ hris/views.py
@@ -4,6 +4,7 @@ from django.views import generic
 
 # Create your views here.
 from hris.models import Employee
+from hris.forms import EmployeeForm
 
 def index(request):
     latest_employees_list = Employee.objects.all()
@@ -18,3 +19,14 @@ class EmployeeListView(generic.ListView):
 class EmployeeDetailView(generic.DetailView):
     model = Employee
 
+def add_employee(request):
+
+    if request.method == 'POST':
+
+        new_employee_form = EmployeeForm(data=request.POST)
+        if  new_employee_form.is_valid():
+                new_employee = new_employee_form.save()
+    else:
+        new_employee_form = EmployeeForm()
+
+    return render(request,hris/add.html,{})
