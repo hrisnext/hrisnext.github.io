ff33aba5d7344d97173342756b220ca45ec8eb0b
alloai <ca365qc@yahoo.com>
2019-07-24 20:55:49 -0600

    add employee

--- hris/admin.py
+++ hris/admin.py
@@ -5,5 +5,5 @@ from hris.models import Employee
 @admin.register(Employee)
 class EmployeeAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'birthdate', 'sin_number','salary','employee_number','hire_date')
-    #list_filter = ('status', 'due_back')
+    list_filter = ('hire_date', 'salary','birthdate')
    
--- hris/urls.py
+++ hris/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('add', views.add_employee, name='add'),
     path('employee/', views.EmployeeListView.as_view(), name='employee'),
-    path('employee/add', views.EmployeeListView.as_view(), name='employee-list'),
-    path('employee/<int:pk>',views.EmployeeCreate.as_view(), name = 'employee-detail'),
+    path('employee/create', views.EmployeeCreate.as_view(), name='employee-add'),
+    path('employee/<int:pk>',views.EmployeeDetailView.as_view(), name = 'employee-detail'),
     path('employee/<int:pk>/', views.EmployeeUpdate.as_view(), name='employee-update'),
     path('employee/<int:pk>/delete/', views.EmployeeDelete.as_view(), name='employee-delete'),
     
