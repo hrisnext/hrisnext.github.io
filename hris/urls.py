8ac69bd12b77e813f41d5946b0ed83bec0f90711
alloai <ca365qc@yahoo.com>
2019-07-24 21:07:32 -0600

    urls changes

--- hris/urls.py
+++ hris/urls.py
@@ -6,10 +6,10 @@ app_name = 'hris'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('add', views.add_employee, name='add'),
-    path('employee/', views.EmployeeListView.as_view(), name='employee'),
-    path('employee/create', views.EmployeeCreate.as_view(), name='employee-add'),
-    path('employee/<int:pk>',views.EmployeeDetailView.as_view(), name = 'employee-detail'),
+    path('add/', views.add_employee, name='add'),
+    path('employee/', views.EmployeeListView.as_view(), name='employee-list'),
+    path('employee/create/', views.EmployeeCreate.as_view(), name='employee-add'),
+    path('employee/<int:pk>/',views.EmployeeDetailView.as_view(), name = 'employee-detail'),
     path('employee/<int:pk>/', views.EmployeeUpdate.as_view(), name='employee-update'),
     path('employee/<int:pk>/delete/', views.EmployeeDelete.as_view(), name='employee-delete'),
     
